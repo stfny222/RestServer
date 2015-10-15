e17ff51f18ebbcc50f2dde03d855def89241bb78
evaldivieso <estefanyval30@gmail.com>
2015-10-15 13:13:25 -0500

    1 commit

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+	res.send('<form method="POST" action="/postit">'+
+		'<input name="user" type="text" />'+
+		'<input name="pwd" type="password" />'+
+		'<input type="submit" value="enviar" />'+
+		'</form>');
+});
+
+/*app.get('/user/:id', function(req, res) {
+  res.send('user ' + req.params.id);
+});*/
+
+/*app.get('/holi?id',function (req,res){
+  res.send('user ' + req.query.id);
+});*/
+
+/*app.post('/postit', function (req,res){
+	//console.log("req.body",req.body);
+	//res.json(req.body.user);
+	res.json({
+		foo: "bar"
+	});
+});*/
+
+app.post('/postit', function (req,res){
+	var dependencies = ['Ingeniería de Sistemas', 
+	'Psicología',
+	'Ingeniería Industrial',
+	'Centro Integral de Educación Continua'];
+	res.json(dependencies);
+});
+
+app.listen(3000);
