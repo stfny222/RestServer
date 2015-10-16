67a7b0ea8ecef0d098bb795de51d1c493ab7840e
evaldivieso <estefanyval30@gmail.com>
2015-10-15 19:55:04 -0500

    puerto 3000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
